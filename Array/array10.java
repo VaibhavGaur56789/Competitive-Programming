92cef682bc210dcdae98d26af4bb1cdc2f1da261
Vaibhav Gaur <vaibhavgaur56789@gmail.com>
2021-05-22 21:30:50 +0530

    Minimise the maximum difference between heights

---
+++ Array/array10.java
@@ -0,0 +1,37 @@
+//Merge 2 sorted arrays without using Extra space
+
+import java.util.*;
+public class array10 {
+    public static void merge(int[] arr1, int[] arr2)
+    {
+        int m = arr1.length;
+        int n = arr2.length;
+
+        for(int i=0; i<m; i++) {
+            if(arr1[i] > arr2[0]) {
+                int temp = arr1[i];
+                arr1[i] = arr2[0];
+                arr2[0] = temp;
+
+                int first = arr2[0];
+
+                int k;
+                for(k=1; k<n && arr2[k] < first; k++) {
+                    arr2[k-1] = arr2[k];
+                }
+                arr2[k-1] = first;
+            }
+        }
+
+    }
+    public static void main (String[] args)
+    {
+        int[] X = { 1, 4, 7, 8, 10 };
+        int[] Y = { 2, 3, 9 };
+
+        merge(X, Y);
+
+        System.out.println("X: " + Arrays.toString(X));
+        System.out.println("Y: " + Arrays.toString(Y));
+    }
+}
